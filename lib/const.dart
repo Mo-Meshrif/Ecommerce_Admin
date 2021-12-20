3165dfb9379dbc79e96c292ec0da794efd5a64b0
unknown <m.meshrif77@gmail.com>
2021-12-20 08:13:42 +0200

    remove settingsView

--- lib/const.dart
+++ lib/const.dart
@@ -5,7 +5,6 @@ import 'views/subViews/homeView/dashboardView.dart';
 import 'views/subViews/homeView/messagesView/messagesView.dart';
 import 'views/subViews/homeView/ordersView/ordersView.dart';
 import 'views/subViews/homeView/productsView/productsView.dart';
-import 'views/subViews/homeView/settingsView.dart';
 import 'package:responsive_table/DatatableHeader.dart';
 import 'package:flutter/material.dart';
 
@@ -17,20 +16,16 @@ List<Map<String, String>> adminItems = [
     'icon': 'assets/home/categories.png',
   },
   {
+    'title': 'Messages',
+    'icon': 'assets/home/messages.png',
+  },
+  {
     'title': 'Customers',
     'icon': 'assets/home/customers.png',
   },
   {
     'title': 'Shops',
     'icon': 'assets/home/shops.png',
-  },
-  {
-    'title': 'Messages',
-    'icon': 'assets/home/messages.png',
-  },
-  {
-    'title': 'Settings',
-    'icon': 'assets/home/settings.png',
   },
   {
     'title': 'Logout',
@@ -55,27 +50,21 @@ List<Map<String, String>> mangerItems = [
     'icon': 'assets/home/products.png',
   },
   {
-    'title': 'Settings',
-    'icon': 'assets/home/settings.png',
-  },
-  {
     'title': 'Logout',
     'icon': 'assets/home/logout.png',
   },
 ];
 List<Widget> adminViews = [
   CategoriesView(),
+  MessagesView(),
   CustomersView(),
   ShopsView(),
-  MessagesView(),
-  SettingsView(),
 ];
 List<Widget> mangerViews = [
   DashboardView(),
   MessagesView(),
   OrdersView(),
   ProductsView(),
-  SettingsView(),
 ];
 List<DatatableHeader> headers = [
   DatatableHeader(
